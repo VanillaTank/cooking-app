63ebe576d69592195515b06df47cd7263537f1ab
VanillaTank <tedien2016@yandex.ru>
2021-07-01 14:35:32 +0800

    add обработку коммента и 3 рецепта

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import data from './data.js'
 
 
-window.onload = () => {showRandomRecipe()}
+window.onload = () => { showRandomRecipe() }
 const btnShowAll = $('#btnShowAll');
 const outMenu = $('.outMenu');
 const outRecipe = $('.outRecipe');
@@ -14,33 +14,38 @@ searchInput.addEventListener('input', (evt) => { onInputSaerchInput(evt) })
 navBtns.forEach(item => { item.addEventListener('click', (evt) => sortingRecipes(evt)) })
 
 btnShowAll.addEventListener('click', () => {
-    if (data.length != 0) {
-        outMenu.innerHTML = '';
-        data.forEach(item => { showRecipes(item) })
-    } else outMenu.innerHTML = '<li class="outMenuItemLiNone">Ничего не найдено</li>'
+  if (data.length != 0) {
+    outMenu.innerHTML = '';
+    data.forEach(item => { showRecipes(item) })
+  } else outMenu.innerHTML = '<li class="outMenuItemLiNone">Ничего не найдено</li>'
 
 })
 
 //----------------------------------------------
 function showRandomRecipe() {
-  if(data.length != 0) {
+  if (data.length != 0) {
     const randomIndex = Math.floor(Math.random() * data.length)
     const targetRecipe = data[randomIndex];
 
     const liSteps = () => {
       const outRecipesSteps = document.createElement('ol')
       outRecipesSteps.className = "outRecipe-steps"
-      for(let i = 0; i < targetRecipe.steps.length; i++) {
+      for (let i = 0; i < targetRecipe.steps.length; i++) {
         outRecipesSteps.innerHTML += `<li>${targetRecipe.steps[i]}</li>`
       }
       return outRecipesSteps
+    }
+
+    let comment = '';
+    if (targetRecipe.comments != '') {
+      comment = `<div class="outRecipe-comments">${targetRecipe.comments}</div>`
     }
 
     const targetRecipeBlock = `
     <div>
         <h3 class="outRecipe-title">${targetRecipe.title}</h3>
         <div class="outRecipe-needs">${targetRecipe.needs.join('<br>')}</div>
-        <div class="outRecipe-comments">${targetRecipe.comments}</div>
+        ${comment}
         ${liSteps().outerHTML}
     </div>`
     outRecipe.innerHTML = targetRecipeBlock;
@@ -48,72 +53,79 @@ function showRandomRecipe() {
 }
 
 function $(el) {
-    return document.querySelector(el);
+  return document.querySelector(el);
 }
+
 function onInputSaerchInput(evt) {
-    //поставить таймер, чтоб не искать на КАЖДЫЙ символ
-    //искать в дате по имени в title
-    //выводить showRecipes(item)
+  //поставить таймер, чтоб не искать на КАЖДЫЙ символ
+  //искать в дате по имени в title
+  //выводить showRecipes(item)
 }
+
 function showRecipes(item) {
-    const outMenuItem = `
+  const outMenuItem = `
         <li id='${item.id}' class="outMenuItemLi">
             <h3>${item.title}</h3>
         </li>`;
 
-    outMenu.innerHTML += outMenuItem;
+  outMenu.innerHTML += outMenuItem;
 
-    const outMenuItemLi = [...document.querySelectorAll('.outMenuItemLi')] || null;
-    outMenuItemLi.forEach(item => {
+  const outMenuItemLi = [...document.querySelectorAll('.outMenuItemLi')] || null;
+  outMenuItemLi.forEach(item => {
 
-        item.addEventListener('click', (evt) => {
-            outMenuItemLi.forEach(it => {it.classList.remove("active")})
-            onClickMenuItem(evt)
-        })
+    item.addEventListener('click', (evt) => {
+      outMenuItemLi.forEach(it => { it.classList.remove("active") })
+      onClickMenuItem(evt)
     })
+  })
 }
 
 function onClickMenuItem(evt) {
-    evt.currentTarget.classList.add("active");
-    const index = evt.currentTarget.id;
-    const targetRecipe = data[index];
+  evt.currentTarget.classList.add("active");
+  const index = evt.currentTarget.id;
+  const targetRecipe = data[index];
 
 
-    const liSteps = () => {
-      const outRecipesSteps = document.createElement('ol')
-      outRecipesSteps.className = "outRecipe-steps"
-      for(let i = 0; i < targetRecipe.steps.length; i++) {
-        outRecipesSteps.innerHTML += `<li>${targetRecipe.steps[i]}</li>`
-      }
-      return outRecipesSteps
+  const liSteps = () => {
+    const outRecipesSteps = document.createElement('ol')
+    outRecipesSteps.className = "outRecipe-steps"
+    for (let i = 0; i < targetRecipe.steps.length; i++) {
+      outRecipesSteps.innerHTML += `<li>${targetRecipe.steps[i]}</li>`
     }
+    return outRecipesSteps
+  }
 
-    const targetRecipeBlock = `
+  let comment = '';
+  if (targetRecipe.comments != '') {
+    comment = `<div class="outRecipe-comments">${targetRecipe.comments}</div>`
+  }
+
+  const targetRecipeBlock = `
     <div>
         <h3 class="outRecipe-title">${targetRecipe.title}</h3>
         <div class="outRecipe-needs">${targetRecipe.needs.join('<br>')}</div>
-        <div class="outRecipe-comments">${targetRecipe.comments}</div>
+        ${comment}
         ${liSteps().outerHTML}
     </div>`
-    outRecipe.innerHTML = targetRecipeBlock;
+  outRecipe.innerHTML = targetRecipeBlock;
 }
 
 function sortingRecipes(evt) {
-    navBtns.forEach(item => { item.classList.remove('chosen') })
-    evt.currentTarget.classList.add("chosen");
-    const targetGroup = evt.currentTarget.textContent; //не очень здоровая штука
-    if (data.length != 0) {
-        let foundedRecipes = [];
-        data.forEach(item => {
-            for (let i = 0; i < item.groups.length; i++) {
-                if (item.groups[i] == targetGroup) foundedRecipes.push(item)
-            }
-        })
-        if (foundedRecipes.length === 0) {
-            outMenu.innerHTML = '<li class="outMenuItemLiNone">Ничего не найдено</li>'
-            return
-        }
-        outMenu.innerHTML = '';
-        foundedRecipes.forEach(item => { showRecipes(item) })
-    } else outMenu.innerHTML = '<li class="outMenuItemLiNone">Ничего не найдено</li>'
+  navBtns.forEach(item => { item.classList.remove('chosen') })
+  evt.currentTarget.classList.add("chosen");
+  const targetGroup = evt.currentTarget.textContent; //не очень здоровая штука
+  if (data.length != 0) {
+    let foundedRecipes = [];
+    data.forEach(item => {
+      for (let i = 0; i < item.groups.length; i++) {
+        if (item.groups[i] == targetGroup) foundedRecipes.push(item)
+      }
+    })
+    if (foundedRecipes.length === 0) {
+      outMenu.innerHTML = '<li class="outMenuItemLiNone">Ничего не найдено</li>'
+      return
+    }
+    outMenu.innerHTML = '';
+    foundedRecipes.forEach(item => { showRecipes(item) })
+  } else outMenu.innerHTML = '<li class="outMenuItemLiNone">Ничего не найдено</li>'
 }
